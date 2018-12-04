5d7c691cac0a57e541749820826bfda4cfa625dd
MD <mr.drobnych@gmail.com>
2018-12-04 11:34:58 +0200

    first commit

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,43 @@
+class ProfilesController < ApplicationController
+before_action :authenticate_user!
+  def new
+  	@profile = Profile.new
+  end
+
+  def create
+  	@profile = current_user.build_profile(profile_params)
+  	if @profile.save
+  		redirect_to root_path
+  	else
+  		render 'new'
+  	end
+  end
+
+  def show
+  	@profile = Profile.find_by(id: params[:id])
+  end
+
+  def edit
+  	@profile = Profile.find_by(id: params[:id])
+  end
+
+  def update
+  	@profile = Profile.find_by(id: params[:id])
+    if @profile.update(profile_params)
+  		redirect_to profile_path(@profile.id)
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+    @profile = Profile.find_by(id: params[:id])
+  	@profile.destroy 
+  	redirect_to root_path
+  end
+
+private 
+  def profile_params
+  	params.require(:profile).permit(:user_id, :nickname, :age, :avatar)
+  end
+end
